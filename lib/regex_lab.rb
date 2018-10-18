390764341e31d925f138b33ad9f8b4f088169b37
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-18 01:23:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  return text.match(/\A[A-Z].+[.?!,]\z/)
+  if text.match(/\A[A-Z].+[.?!,]\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/\d/).length == 10
+    true
+  else
+    false
+  end
 end
